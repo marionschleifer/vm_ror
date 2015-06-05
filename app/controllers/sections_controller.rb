2e471f846f0436ec5089e33d639772ca87614405
Marion Schleifer <marion.schleifer@gmail.com>
2015-06-05 13:57:24 +0200

    section empty?
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -61,6 +61,10 @@ class SectionsController < ApplicationController
     end
   end
 
+  def empty?
+    [true, false].sample
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_section
