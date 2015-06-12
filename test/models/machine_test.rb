0658ffd4032addde25a12229c51ca42e56348e31
Marion Schleifer <marion.schleifer@gmail.com>
2015-06-12 16:59:09 +0200

    unit tests machine
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- test/models/machine_test.rb
+++ test/models/machine_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class MachineTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @machine = Machine.new
+    @machine.name = "My machine"
+    @machine.positions = 3
+    @machine.current_position = 1
+  end
+
+  test "turn" do
+    assert 1, @machine.current_position
+    @machine.turn!
+    assert 2, @machine.current_position
+  end
+
+  test "turn back to first position" do
+    3.times do
+      @machine.turn!
+    end
+    assert 1, @machine.current_position
+  end
+
 end
